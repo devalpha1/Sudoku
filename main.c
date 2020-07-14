c1abb784686d7aa2a0dc7532751d07bfc96e8e4b
devalpha1 <53270833+devalpha1@users.noreply.github.com>
2020-07-14 16:11:07 +0400

    tableau de 9 case
    Basic

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int menu()
 {
@@ -14,8 +15,86 @@ int menu()
 
 }
 
+void modificationTableau( char *tableau)
+{
+    tableau[4] = '1';
+}
+
+char fromIntToChar(int number)
+{
+    if ( number == 1)
+    {
+        return  '1';
+    }
+    else if( number == 2)
+    {
+        return '2';
+    }
+    else if( number == 3)
+    {
+        return '3';
+    }
+    else if( number == 4)
+    {
+        return  '4';
+    }
+    else if( number == 5)
+    {
+        return '5';
+    }
+    else if( number == 6)
+    {
+        return '6';
+    }
+    else if( number == 7)
+    {
+        return  '7';
+    }
+    else if( number == 8)
+    {
+        return  '8';
+    }
+    else if( number == 9)
+    {
+        return  '9';
+    }
+
+}
+
+
 int main()
 {
+    char tableau[9] = { '1', '2', '3',
+                        '4', '5', '6',
+                        '7', '8', '9' };
+    int i = 0;
+
+    srand(time(NULL));
+
+    tableau[0] = rand() % 9 + 1;
+    tableau[0] = fromIntToChar(tableau[0]);
+
+    for( i = 1 ; i < 9 ; i++ )
+    {
+        tableau[i] = rand() % 9 + 1;
+
+        tableau[i] = fromIntToChar(tableau[i]);
+    }
+
+    for ( i = 0 ; i < 9 ; i++ )
+    {
+        printf("%c : %d\n\n", tableau[i], tableau[i]); // affiche 5 normalement
+    }
+
+    modificationTableau(tableau);
+
+    printf("%c : %d\n\n",tableau[4], tableau[4]);
+
+    scanf("%c", &tableau[4]);
+
+    printf("%c : %d\n\n", tableau[4], tableau[4]);
+
+
     do
     {
 
@@ -23,5 +102,8 @@ int main()
 
 
 
+
     return 0;
 }
+
+
